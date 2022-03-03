96a2487101f31acb06290c6dd1592a28ed888123
Pooyan Soltani <pooyansoltani@gmail.com>
2022-03-03 19:05:59 +0330

    added task schema refactoring task resource added validation

---
+++ schemas/task.py
@@ -0,0 +1,16 @@
+from marshmallow import Schema, fields, validate, pre_load
+
+
+class TaskSchema(Schema):
+    class Meta:
+        load_only = ()
+        dump_only = ()
+
+    text = fields.String(validate=validate.Length(min=1, max=100))
+    finished = fields.Boolean()
+
+    @pre_load
+    def strip_text(self, item, many, **kwargs):
+        if "text" in item:
+            item["text"] = item["text"].strip()
+        return item
---
+++ schemas/user.py
@@ -0,0 +1,26 @@
+from marshmallow import Schema, fields, validate, pre_load
+
+
+class UserSchema(Schema):
+    class Meta:
+        load_only = ()
+        dump_only = ()
+
+    username = fields.Str(required=True, validate=validate.Length(min=3, max=20))
+    name = fields.Str(validate=validate.Length(min=3, max=20))
+    last = fields.Str(validate=validate.Length(min=3, max=20))
+    password = fields.Str(required=True, validate=validate.Length(min=3, max=20))
+
+    # strip input data
+    @pre_load(
+        pass_many=True,
+    )
+    def strip_data(self, item, many, **kwargs):
+        if "username" in item:
+            item["username"] = item["username"].strip()
+        if "name" in item:
+            item["name"] = item["name"].strip()
+        if "last" in item:
+            item["last"] = item["last"].strip()
+
+        return item
